03706be6707663c7101c4d96e8f1d6c94181837a
hayknazaryann <nazaryanhayk4@gmail.com>
2024-03-14 12:13:24 +0400

    read me

--- config/vivarolls.php
+++ config/vivarolls.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api' => env('VIVAROLLS_URL'),
+    'api' => env('VIVAROLLS_URL', 'https://vivarolls.magic-of-numbers.ru:8686'),
     'username' => env('VIVAROLLS_USERNAME'),
     'password' => env('VIVAROLLS_PASSWORD')
 ];
